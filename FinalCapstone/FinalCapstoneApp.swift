59c563bdd69a4356cff9b6e9bb8e621e764d65c3
Fahad Matlagitu <matlagitu@gmail.com>
2023-12-20 10:13:09 +0300

    completed App

---
+++ FinalCapstone/FinalCapstoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalCapstoneApp.swift
+//  FinalCapstone
+//
+//  Created by Fahad Matlagitu on 20/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct FinalCapstoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
